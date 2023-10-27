b2fe1c0021ee06f10bf4bd28a43c5c1e7948069e
Greg Osetrov <shadow-greg@yandex.ru>
2023-10-27 11:39:38 +0300

    добавили рабочие методы

--- src/main/java/ru/gb/Main.java
+++ src/main/java/ru/gb/Main.java
@@ -1,7 +1,38 @@
 package ru.gb;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(4);
+        list2.add(5);
+        list2.add(6);
+
+        double average1 = calculateAverage(list1);
+        double average2 = calculateAverage(list2);
+
+        if (average1 > average2) {
+            System.out.println("Первый список имеет большее среднее значение");
+        } else if (average1 < average2) {
+            System.out.println("Второй список имеет большее среднее значение");
+        } else {
+            System.out.println("Средние значения равны");
+        }
+    }
+
+    private static double calculateAverage(List<Integer> list) {
+        int sum = 0;
+        for (int num : list) {
+            sum += num;
+        }
+        return (double) sum / list.size();
     }
 }
+
